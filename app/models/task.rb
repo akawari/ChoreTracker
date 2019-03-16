dfede0c7589e9eae17adc93ee01c34ed93ac929f
Ahmad Al-Kawari <akawari@andrew.cmu.edu>
2019-03-16 13:39:55 +0000

    Testing for tasks have been passed successfully

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ApplicationRecord
+    #Relationships Testing:
     has_many :chores
     has_many :children, through: :chores
+    
+    #Validation Testing:
+    validates :name, presence: true
+    validates :points, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    
+    #Scopes Testing:
+    scope :alphabetical, -> {order('name asc')}
+    scope :active, -> {where('active = ?', true)}
 end
